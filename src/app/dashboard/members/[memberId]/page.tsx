453ff620ead16fa4b8948a7272fe3629ed1c2e72
bruno-calasans <brunogstsho@gmail.com>
2025-05-17 17:25:18 -0300

    add loading state for members/[memberId] page

--- src/app/dashboard/members/[memberId]/page.tsx
+++ src/app/dashboard/members/[memberId]/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import Loader from "@/components/custom/Loader";
 import MemberPageBreadcrumbs from "@/components/dashboard/Members/MemberPageBreadcrumbs";
 import MemberTabs from "@/components/dashboard/Members/MemberTabs";
 import ContentContainer from "@/components/general/ContentContainer";
 import { useMemberStore } from "@/store/memberStore";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type MemberInfoPageProps = {
   params: Promise<{ memberId: string }>;
 };
 
 export default function MemberInfoPage({ params }: MemberInfoPageProps) {
+  const [loading, setLoading] = useState(true);
   const { getMemberById, setSelectedMember } = useMemberStore();
   const router = useRouter();
 
@@ -21,11 +23,19 @@ export default function MemberInfoPage({ params }: MemberInfoPageProps) {
     const foundMember = getMemberById(memberId);
     if (!foundMember) return router.push("/dashboard/members");
     setSelectedMember(foundMember);
+    setLoading(false);
   };
 
   useEffect(() => {
     loadMember();
   }, []);
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center flex-1">
+        <Loader text="Procurando membro" />
+      </div>
+    );
 
   return (
     <ContentContainer>
