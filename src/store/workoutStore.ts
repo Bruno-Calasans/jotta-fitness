784d819110e589531306c365aa1f7e6c8ee6271a
bruno-calasans <brunogstsho@gmail.com>
2025-06-20 16:42:01 -0300

    merge main and dashboard

--- src/store/workoutStore.ts
+++ src/store/workoutStore.ts
@@ -55,7 +55,7 @@ export const useWorkoutStore = create<WorkoutState>()(
         },
         removeWorkout(workoutId) {
           const filtedWorkouts = get().workouts.filter(
-            (w) => w.id != workoutId
+            (w) => w.id != workoutId,
           );
           set((state) => ({
             workouts: filtedWorkouts,
@@ -140,7 +140,7 @@ export const useWorkoutStore = create<WorkoutState>()(
 
           return keyword && keyword != ""
             ? workouts.filter((w) =>
-                w.name.toLowerCase().includes(keyword.toLowerCase())
+                w.name.toLowerCase().includes(keyword.toLowerCase()),
               )
             : workouts;
         },
@@ -172,7 +172,7 @@ export const useWorkoutStore = create<WorkoutState>()(
       {
         name: "workout-storage",
         storage: createJSONStorage(() => localStorage),
-      }
-    )
-  )
+      },
+    ),
+  ),
 );
